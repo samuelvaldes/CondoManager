1334fd20a2d5be17621afa3ba3ee8fcf8fe7a610
Samuel Valdes Samperio <svaldes_samperio@hotmail.com>
2022-02-02 21:45:03 -0500

    Se Instala Firebase Messaging y codifica la petición de permisos

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
 import 'react-native-gesture-handler';
-import { View, Text, LogBox } from 'react-native';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import messaging from '@react-native-firebase/messaging';
+
 import Navigator from './src/navigation/Navigator';
 import { AuthProvider } from './src/context/AuthContext';
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission({
+    announcement: true,
+  });
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
 
 export default function App() {
   LogBox.ignoreLogs([
     "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
     "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
   ]);
+
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
 
   return (
     <NavigationContainer>
